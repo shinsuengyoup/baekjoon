82b72b6f1f5094f882d9615f0e5fe3429a372cb4
shinsuengyoup <74814396+shinsuengyoup@users.noreply.github.com>
2023-01-02 19:18:14 +0900

    Create 좌표압축.cpp

---
+++ backjoon/backjoon/좌표압축.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main() {
+	int N,number;
+	cin >> N;
+	vector<int> originVec, copyVec, compareVec;
+	for (int i = 0; i < N; i++) {
+		cin >> number;
+		originVec.push_back(number);
+	}
+	copyVec = originVec;
+
+	// Á¤·Ä
+	sort(copyVec.begin(), copyVec.end());
+	
+	int myself = copyVec[0];
+	compareVec.push_back(myself);
+
+	for (int i = 0; i < N; i++) {
+		if (copyVec[i] != myself) {
+			compareVec.push_back(copyVec[i]);
+			myself = copyVec[i];
+		}
+	}
+	
+	for (int i = 0; i < N; i++) {
+		cout << lower_bound(compareVec.begin(), compareVec.end(), originVec[i]) - compareVec.begin() << ' ';
+	}
+
+	return 0;
+}
